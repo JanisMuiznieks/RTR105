74f278ae9e96c0fc5feef3b41ac06f224d387d8b
Janis <janis.muiznieks8@gmail.com>
2018-10-08 18:47:58 +0300

    20181010_13_33

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ PATH=$PATH/ - izveidot skriptu
 mv - parvietot
 ping 213.175.92.22 - parbaudit internetu
 
+Python
+vars() izprinte f-ju iespejas
+__doc__ dokumente f-ju
+print() izprinte tekstu
+type(..) izprinte datu tipu
---
+++ dgr_20181010.py
@@ -0,0 +1,35 @@
+Python 2.7.6 (default, Jun 22 2015, 18:00:18) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(x)
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> x = 12
+>>> print (x)
+12
+>>> print('hello')
+hello
+>>> print('hello world')
+hello world
+>>> int
+<type 'int'>
+>>> float
+<type 'float'>
+>>> a =5
+>>> b = 4
+>>> c = 5
+>>> c
+5
+>>> d = a% b
+>>> d
+1
+>>> dgr_20281010.py
+
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    dgr_20281010.py
+NameError: name 'dgr_20281010' is not defined
+>>> 
